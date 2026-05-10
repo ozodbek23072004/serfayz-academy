9d8ee8372d042381144d43d69d6d212b30359c5d
Ozodbek <ozodbek@kali>
2026-05-10 16:15:33 +0500

    chore: update domain to serfayz.uz

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:8080',
   'http://127.0.0.1:8080',
-  'http://bek2.vps.webdock.cloud',
+  'http://serfayz.uz',
+  'http://www.serfayz.uz',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
